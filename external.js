b6e968167589d25b0c9cc507ae707b5742d2aa6d
Matt Withey <matt.withey@outlook.com>
2022-11-24 19:02:23 +1000

    computer turn functionality.

---
+++ external.js
@@ -0,0 +1,36 @@
+const playerChoice = 'Rock';
+let computerChoice = getComputerChoice();
+
+playRound(playerChoice, computerChoice);
+
+function getComputerChoice() {
+    let choice;
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            choice = 'Rock';
+            break;
+
+        case 1:
+            choice = 'Paper';
+            break;
+
+        case 2:
+            choice = 'Scissors';
+            break;
+    }
+    return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log('Player Selection: ', playerSelection)
+    console.log('Computer Selection: ', computerSelection)
+    return (playerSelection == 'Scissors' && computerSelection == 'Paper') ? console.log('win!')
+        : (playerSelection == 'Scissors' && computerSelection == 'Rock') ? console.log('lose!')
+            : (playerSelection == 'Paper' && computerSelection == 'Rock') ? console.log('win!')
+                : (playerSelection == 'Paper' && computerSelection == 'Scissors') ? console.log('lose!')
+                    : (playerSelection == 'Rock' && computerSelection == 'Scissors') ? console.log('win!')
+                        : (playerSelection == 'Rock' && computerSelection == 'Paper') ? console.log('lose!')
+                            : console.log('draw!');
+}
+
+
